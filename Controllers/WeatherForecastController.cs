f3c8bdeb861a6e8ce9ad00a151e4c4d22a3411ac
TomWang <907465687@qq.com>
2021-01-17 21:48:10 +0800

    first push

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebAPINew.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("GetName")]
+        public ActionResult GetName() {
+            string res = "Hello, the API is success";
+            return Content(res);
+        }
+
+        [HttpGet]
+        [Route("GetAge")]
+        [Route("GetAge/age")]
+        public ActionResult GetAge(int age)
+        {
+            string res = $"Hello, Tom Wang is thirty years ({age}) old";
+            return Content(res);
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# WebAPITest
+# WebAPI
